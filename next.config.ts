567b450a2c3f4f45bf563f737c7b4267eceda8d6
gmitsolution3 <gmitsolution3@gmail.com>
2026-03-26 10:44:15 +0600

    added headers config in next config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,29 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["res.cloudinary.com", "i.ibb.co.com"],
   },
+  async headers() {
+    return [
+      {
+        source: "/api/:path*", // Matches all API routes
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://www.graphicsmultimedia.net",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value:
+              "X-CSRF-Token, X-Requested-With, Accept, Content-Type, Authorization",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
